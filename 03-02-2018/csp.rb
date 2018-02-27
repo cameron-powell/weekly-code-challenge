9fbb7d72e7eb7fa6a6617b67d671dd31f405a999
Cameron Powell <cameronspowell@gmail.com>
2018-02-26 21:10:53 -0500

    solve challenge

---
+++ 03-02-2018/csp.rb
@@ -0,0 +1,31 @@
+# Read in the grid.
+grid = []
+6.times do
+  row = gets.split.map { |val| val.to_i }
+  grid.push row
+end
+
+#  Calculates the sum of an hourglass in the grid
+#  where the index of the top left element of the
+#  hourglass is at [row][col]
+def hourglass_sum(grid, row, col)
+  sum = 0
+  # Sum top and bottom rows
+  (0..2).each do |shift|
+    sum += grid[row][col+shift]
+    sum += grid[row+2][col+shift]
+  end
+  # Add in middle
+  sum += grid[row+1][col+1]
+end
+
+# Calculate the maximum hourglass sum
+max_sum = hourglass_sum grid, 0, 0
+(0..3).each do |row|
+  (0..3).each do |col|
+    current_hourglass_sum = hourglass_sum grid, row, col
+    max_sum = (current_hourglass_sum > max_sum) ? current_hourglass_sum : max_sum
+  end
+end
+
+puts max_sum
