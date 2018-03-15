7201a955122ff8aa237f7409cf3b7cfc8339ead1
Cameron Powell <cameronspowell@gmail.com>
2018-03-14 22:30:01 -0400

    knock a line off

--- 03-16-2018/csp.rb
+++ 03-16-2018/csp.rb
@@ -1,4 +1,3 @@
-house_left, house_right = gets.split.map &:to_i
-apple_tree, orange_tree = gets.split.map &:to_i; gets
+house_left, house_right, apple_tree, orange_tree = gets.split.map(&:to_i) + gets.split.map(&:to_i); gets
 puts gets.split.map { |apple| (apple_tree + apple.to_i >= house_left && apple_tree + apple.to_i <= house_right) ? 1 : 0 }.reduce(:+)
 puts gets.split.map { |orange| (orange_tree + orange.to_i >= house_left && orange_tree + orange.to_i <= house_right) ? 1 : 0 }.reduce(:+)
