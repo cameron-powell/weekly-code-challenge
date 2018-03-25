d7548d5d199eccf786816bc0532134e0c38041de
Deborah Lynn Ferguson <dferguson41@gatech.edu>
2018-03-25 00:15:07 -0400

    in line for each loops

--- 03-23-2018/dlf.rb
+++ 03-23-2018/dlf.rb
@@ -1,7 +1,5 @@
 gets; scores = gets.split.map(&:to_i)
 min, max, min_count, max_count = scores[0], scores[0], 0, 0
-scores.each do |score|
-  min, min_count = score, min_count+1 if score < min
-  max, max_count = score, max_count+1 if score > max
-end
+scores.each { |score| min, min_count = score, min_count+1 if score < min }
+scores.each { |score| max, max_count = score, max_count+1 if score > max }
 print(max_count," ",min_count)
