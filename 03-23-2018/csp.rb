51ec86b028ec7550276a9325c8b0c968325f394d
Cameron Powell <cameronspowell@gmail.com>
2018-03-25 00:09:29 -0400

    Removed whitespace

--- 03-23-2018/csp.rb
+++ 03-23-2018/csp.rb
@@ -1,9 +1,7 @@
 gets; scores = gets.split.map(&:to_i)
 max, min = [scores[0], 0], [scores[0], 0]
-
 (1...scores.length).each do |index|
   max = (scores[index] > max[0]) ? [scores[index], max[1]+1] : max
   min = (scores[index] < min[0]) ? [scores[index], min[1]+1] : min
 end
-
 puts "#{max[1]} #{min[1]}"
