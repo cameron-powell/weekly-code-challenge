aa3c35379f9543c196b793642e11a7fff91f6faa
Cameron Powell <cameronspowell@gmail.com>
2018-03-27 23:32:50 -0400

    Create sum instead of pairs in list comprehension

--- 03-30-2018/csp.py
+++ 03-30-2018/csp.py
@@ -1,5 +1,4 @@
 from functools import reduce
 n, k = map(lambda x: int(x), input().split())
 numbers = list(map(lambda x: int(x), input().split()))
-count = reduce((lambda x, y: x + y), map(lambda pair: 1 if ((pair[0] + pair[1]) % k == 0) else 0, [(numbers[i], numbers[j]) for j in range(1,n) for i in range(0, j) if i < j]))
-print(count)
+print(reduce((lambda x, y: x + y), map(lambda val: 1 if (val % k == 0) else 0, [numbers[i]+numbers[j] for j in range(1,n) for i in range(0, j) if i < j])))
