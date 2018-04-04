822cc487f4ec3c7152e1a3d4822911b2f38996ef
Debbie <dferguson41@gatech.edu>
2018-04-03 23:12:25 -0400

    shortened code

--- 04-06-2018/dlf.py
+++ 04-06-2018/dlf.py
@@ -1,7 +1,4 @@
 n,k = map(int,input().split())
 prices = list(map(int,input().split()))
-cost = int(input())
-
-fair = int((sum(prices) - prices[k])/2)
-difference = cost - fair
+difference = int(input()) - int((sum(prices) - prices[k])/2)
 print("Bon Appetit" if difference==0 else difference)
